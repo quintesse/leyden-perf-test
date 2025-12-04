f02eabdf8540517a5c36169705f580e76c25d6a3
Tako Schotanus <tako@codejive.org>
2025-12-04 11:41:30 +0100

    fix: make sure we can create volume binds with SELinux enabled
    Lots of peopel have issues with docker/podman volume binds when SELinux
    is enabled. Adding the :z option should fix that.

--- src/scripts/tests/sqpc/shared-vars.sh
+++ src/scripts/tests/sqpc/shared-vars.sh
@@ -5,4 +5,4 @@ export REPO_URL="https://github.com/quarkusio/spring-quarkus-perf-comparison.git
 
 export PG_CONTAINER_NAME="fruits_db"
 export PG_INITDB_PATH="${TEST_APPS_DIR}/${REPO_NAME}/scripts/dbdata"
-export POSTGRES_ARGS="-v ${PG_INITDB_PATH}:/docker-entrypoint-initdb.d/ -p 5432:5432 -e POSTGRES_USER=fruits -e POSTGRES_PASSWORD=fruits -e POSTGRES_DB=fruits"
+export POSTGRES_ARGS="-v ${PG_INITDB_PATH}:/docker-entrypoint-initdb.d/:z -p 5432:5432 -e POSTGRES_USER=fruits -e POSTGRES_PASSWORD=fruits -e POSTGRES_DB=fruits"
