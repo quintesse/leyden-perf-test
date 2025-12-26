e9a71fafd7c0a88aa6dd21b694dfe2ee42e95192
María Arias de Reyna Domínguez <mariasde@ibm.com>
2025-12-26 10:32:35 +0100

    feat: New test using https://github.com/ionutbalosin/jvm-performance-benchmarks/

---
+++ src/scripts/tests/jpbrw/shared-vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export REPO_NAME="jpbrw"
+export REPO_BENCHMARK_URL="https://github.com/ionutbalosin/jvm-performance-benchmarks.git"
+export REPO_WRAPPER_URL="https://github.com/Delawen/jvm-performance-benchmarks-rest-wrapper.git"
