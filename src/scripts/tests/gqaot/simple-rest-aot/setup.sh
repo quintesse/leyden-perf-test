cf93cb21ebc2b05eea267017fdecd1d94aa3d009
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-02-05 16:25:29 +0100

    fix: no need to duplicate clean package

--- src/scripts/tests/gqaot/simple-rest-aot/setup.sh
+++ src/scripts/tests/gqaot/simple-rest-aot/setup.sh
@@ -6,5 +6,5 @@ source "${TEST_SRC_DIR}"/scripts/buildfuncs.sh
 source "${TEST_SUITE_DIR}/shared-vars.sh"
 
 require_java "25+"
-compile_maven "${REPO_NAME}/quarkus-simple-rest-aot-converters-issue" "clean package -Dquarkus.package.jar.appcds.enabled=true -Dquarkus.package.jar.appcds.use-aot=true"
+compile_maven "${REPO_NAME}/quarkus-simple-rest-aot-converters-issue" "-Dquarkus.package.jar.appcds.enabled=true -Dquarkus.package.jar.appcds.use-aot=true"
 copy_build_artifacts "${REPO_NAME}/quarkus-simple-rest-aot-converters-issue" "simple-rest-aot" "target/code-with-quarkus-1.0.0-SNAPSHOT-runner.jar" "target/quarkus-artifact.properties" "target/lib"
