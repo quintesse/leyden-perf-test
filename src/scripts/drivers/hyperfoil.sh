48e8ecfa62616dc89112ef43e41ea974c3ac5aa6
María Arias de Reyna Domínguez <726590+Delawen@users.noreply.github.com>
2026-02-18 09:59:20 +0100

    This was debugging, oops!

--- src/scripts/drivers/hyperfoil.sh
+++ src/scripts/drivers/hyperfoil.sh
@@ -32,14 +32,6 @@ URL="http:\/\/localhost:8080"
 sed -e "s/^/$URL/" "${TEST_SUITE_DIR}/urls.txt" > $URL_FILE
 DURATION=$((TOTAL_REQ/RATE))
 
-URLS=""
-while IFS= read -r p || [ -n "$p" ]; do
-    URLS="${URLS}${p} "
-done < $URL_FILE
-URLS="${URLS}"
-
-echo "${URLS}"
-
 echo "${preamble[@]}" "jbang src/scripts/drivers/HyperfoilWrk.java -R ${RATE} -d ${DURATION}s -o ${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.csv -f ${URL_FILE}"
 "${preamble[@]}" jbang src/scripts/drivers/HyperfoilWrk.java -R "${RATE}" -d "${DURATION}"s -o "${TEST_OUT_DIR:-.}"/"${TEST_TEST_RUNID}".csv -f "${URL_FILE}"
 
