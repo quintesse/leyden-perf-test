f8b57ae7f619f8df5e0c77a0f5f44ab176f0aff1
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-02-26 11:38:27 +0100

    feat: Using Quarkus 3.32.0

--- src/scripts/tests/gqaot/quarkus-hibernate-orm-spacefox/setup.sh
+++ src/scripts/tests/gqaot/quarkus-hibernate-orm-spacefox/setup.sh
@@ -8,6 +8,9 @@ source "${TEST_SUITE_DIR}/shared-vars.sh"
 sed 's/quarkus-spacefox/gqaot/g' "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/src/main/resources/application.properties" > "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/src/main/resources/application.properties.new"
 mv "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/src/main/resources/application.properties.new" "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/src/main/resources/application.properties"
 
+sed 's/999-SNAPSHOT/3.32.0/g' "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/pom.xml" > "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/pom.xml.2"
+mv "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/pom.xml.2" "${TEST_APPS_DIR}/${REPO_NAME}/quarkus-hibernate-orm-spacefox/pom.xml"
+
 require_java "25+"
 compile_maven "${REPO_NAME}/quarkus-hibernate-orm-spacefox" "-Dquarkus.package.jar.type=aot-jar -Dquarkus.package.jar.appcds.use-aot=true"
 copy_build_artifacts "${REPO_NAME}/quarkus-hibernate-orm-spacefox" "quarkus-hibernate-orm-spacefox" "target/quarkus-app/app" "target/quarkus-app/lib" "target/quarkus-app/quarkus" "target/quarkus-app/quarkus-app-dependencies.txt" "target/quarkus-app/quarkus-run.jar"
