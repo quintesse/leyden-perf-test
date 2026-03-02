dd71446c01b0c23e54505504f067e1c13b927c2d
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-03-02 12:26:05 +0100

    fix: ohac also fixing bug mentioned in https://github.com/quintesse/leyden-perf-test/pull/34

--- src/scripts/drivers/ohac.sh
+++ src/scripts/drivers/ohac.sh
@@ -16,10 +16,16 @@ fi
 # Remove existing OHA database file, in case it exists
 rm -f "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-oha.db" > /dev/null 2>&1 || true
 
-if [[ -v TEST_DRIVER_OHA_RATE_LIMIT && -n "$TEST_DRIVER_OHA_RATE_LIMIT"	]] ; then
-	RATE_ARG=("-q" "$TEST_DRIVER_OHA_RATE_LIMIT")
+if [[ -v TEST_PERF_CNT && -n "$TEST_PERF_CNT"	]] ; then
+	TOTAL_REQ=${TEST_PERF_CNT}
 else
-	RATE_ARG=()
+	TOTAL_REQ=10000
+fi
+
+if [[ -v TEST_DRIVER_OHA_RATE_LIMIT && -n "$TEST_DRIVER_OHA_RATE_LIMIT"	]] ; then
+  RATE=${TEST_DRIVER_OHA_RATE_LIMIT}
+else
+  RATE=1000
 fi
 
 # Prepare list of urls to use
@@ -27,6 +33,7 @@ URL_FILE="${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-urls.txt"
 rm -f "$URL_FILE" > /dev/null 2>&1 || true
 URL="http:\/\/host.docker.internal:8080"
 sed -e "s/^/$URL/" "${TEST_SUITE_DIR}/urls.txt" > $URL_FILE
+DURATION=$((TOTAL_REQ/RATE))
 
 # Prepare command prefix if CPU affinity is to be set
 cpuopts=()
@@ -40,7 +47,7 @@ else
 	HOST="host-gateway"
 fi
 
-cmd="${TEST_ENGINE} run -t --rm ${cpuopts[*]} --add-host=host.docker.internal:$HOST -v ${TEST_OUT_DIR:-.}:/test-results:z ghcr.io/hatoo/oha -n ${TEST_PERF_CNT:-10000} ${RATE_ARG[*]} -u ms --no-tui --output-format json -o /test-results/${TEST_TEST_RUNID}-oha.json --db-url /test-results/${TEST_TEST_RUNID}-oha.db  --urls-from-file $URL_FILE"
+cmd="${TEST_ENGINE} run -t --rm ${cpuopts[*]} --add-host=host.docker.internal:$HOST -v ${TEST_OUT_DIR:-.}:/test-results:z ghcr.io/hatoo/oha -q ${RATE} -z ${DURATION}s -c 50 -u ms --latency-correction -t=10s --no-tui --output-format json -o /test-results/${TEST_TEST_RUNID}-oha.json --db-url /test-results/${TEST_TEST_RUNID}-oha.db  --urls-from-file $URL_FILE"
 echo "   - Driver command: ${cmd}"
 
-MSYS_NO_PATHCONV=1 ${TEST_ENGINE} run -t --rm "${cpuopts[@]}" "--add-host=host.docker.internal:$HOST" -v "${TEST_OUT_DIR:-.}:/test-results:z" ghcr.io/hatoo/oha -n "${TEST_PERF_CNT:-10000}" "${RATE_ARG[@]}" -u ms --no-tui --output-format json -o "/test-results/${TEST_TEST_RUNID}-oha.json" --db-url "/test-results/${TEST_TEST_RUNID}-oha.db"  --urls-from-file "${URL_FILE}"
+MSYS_NO_PATHCONV=1 ${TEST_ENGINE} run -t --rm "${cpuopts[@]}" "--add-host=host.docker.internal:$HOST" -v "${TEST_OUT_DIR:-.}:/test-results:z" ghcr.io/hatoo/oha -q "${RATE}" -z "${DURATION}"s -c 50 -u ms --latency-correction -t=10s --no-tui --output-format json -o "/test-results/${TEST_TEST_RUNID}-oha.json" --db-url "/test-results/${TEST_TEST_RUNID}-oha.db"  --urls-from-file "${URL_FILE}"
