903eaf1d269edb25eaef5d860748f45f2864938b
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-04-08 15:35:19 +0200

    Scripts to run a prepare|run command on drivers

--- src/scripts/drivers/_template.sh
+++ src/scripts/drivers/_template.sh
@@ -4,7 +4,17 @@
 
 set -euo pipefail
 
-# Run performance tests
-# ... put your code to run performance tests here ...
-
 # It must make use of TEST_DRIVER_RATE_LIMIT env var to set rate limit (requests per second) and TEST_PERF_CNT env var to set number of requests.
+
+case "$1" in
+	prepare)
+		# Perform any work required before any tests in the suite are run
+		;;
+	run)
+    # ... put your code to run performance tests here ...
+		;;
+	*)
+		echo "Usage: $0 {prepare|run}"
+		exit 1
+		;;
+esac
--- src/scripts/drivers/hyperfoil.sh
+++ src/scripts/drivers/hyperfoil.sh
@@ -6,42 +6,57 @@ set -euo pipefail
 
 source "${TEST_SRC_DIR}/scripts/appfuncs.sh"
 
-# Prepare command prefix if CPU affinity is to be set
-declare -a preamble=()
-if [[ -v HARDWARE_CONFIGURED && "$HARDWARE_CONFIGURED" == true && -v TEST_DRIVER_CPUS && -n "${TEST_DRIVER_CPUS}" ]]; then
-	preamble=("taskset" "-c" "$TEST_DRIVER_CPUS")
-fi
 
-if [[ -v TEST_PERF_CNT && -n "$TEST_PERF_CNT"	]] ; then
-	TOTAL_REQ="$TEST_PERF_CNT"
-else
-	TOTAL_REQ=10000
-fi
+CMD=$1
+	
+case "${CMD}" in
+	prepare)
+		# Prepare command prefix if CPU affinity is to be set
+		declare -a preamble=()
+		if [[ -v HARDWARE_CONFIGURED && "$HARDWARE_CONFIGURED" == true && -v TEST_DRIVER_CPUS && -n "${TEST_DRIVER_CPUS}" ]]; then
+			preamble=("taskset" "-c" "$TEST_DRIVER_CPUS")
+		fi
 
-if [[ -v TEST_DRIVER_RATE_LIMIT && -n "$TEST_DRIVER_RATE_LIMIT"	]] ; then
-  RATE="$TEST_DRIVER_RATE_LIMIT"
-else
-  RATE=1000
-fi
+		if [[ -v TEST_PERF_CNT && -n "$TEST_PERF_CNT"	]] ; then
+			TOTAL_REQ="$TEST_PERF_CNT"
+		else
+			TOTAL_REQ=10000
+		fi
 
-DURATION=$((TOTAL_REQ/RATE))
+		if [[ -v TEST_DRIVER_RATE_LIMIT && -n "$TEST_DRIVER_RATE_LIMIT"	]] ; then
+		RATE="$TEST_DRIVER_RATE_LIMIT"
+		else
+		RATE=1000
+		fi
 
-URLS_FILE="${TEST_TEST_DIR}/urls.txt"
-if [[ ! -f "$URLS_FILE" ]]; then
-	URLS_FILE="${TEST_SUITE_DIR}/urls.txt"
-	if [[ ! -f "$URLS_FILE" ]]; then
-		echo "ERROR: URLs file not found: $URLS_FILE"
+		DURATION=$((TOTAL_REQ/RATE))
+
+		URLS_FILE="${TEST_TEST_DIR}/urls.txt"
+		if [[ ! -f "$URLS_FILE" ]]; then
+			URLS_FILE="${TEST_SUITE_DIR}/urls.txt"
+			if [[ ! -f "$URLS_FILE" ]]; then
+				echo "ERROR: URLs file not found: $URLS_FILE"
+				exit 1
+			fi
+		fi
+
+		# Prepare list of urls to use
+		URLS_FIXED_FILE="${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-urls.txt"
+		rm -f "$URLS_FIXED_FILE" > /dev/null 2>&1 || true
+		URL="http:\/\/localhost:8080"
+		sed -e "s/^/$URL/" "$URLS_FILE" > "$URLS_FIXED_FILE"
+
+		sleep 1 && pidstat -t -p $(pgrep -f HyperfoilWrk) 1  > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-hyperfoil-pidstat.log" &
+
+		echo "${preamble[*]} jbang --java-options=\"-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0\" --java-options=\"-Xmx1G\" --java-options=\"-Xms1G\" --java-options=\"-XX:+UseParallelGC\" --java-options=\"-XX:+AlwaysPreTouch\" src/scripts/drivers/HyperfoilWrk.java -R ${RATE} -d ${DURATION}s -c 50 -o ${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.csv -f ${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" -p "${TEST_OUT_DIR:-.}"/time-to-8080.csv
+		"${preamble[@]}" jbang --java-options="-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0" --java-options="-Xmx1G" --java-options="-Xms1G" --java-options="-XX:+UseParallelGC" --java-options="-XX:+AlwaysPreTouch" src/scripts/drivers/HyperfoilWrk.java -R "${RATE}" -d "${DURATION}"s -t 1 -o "${TEST_OUT_DIR:-.}"/"${TEST_TEST_RUNID}".csv -f "${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" -p "${TEST_OUT_DIR:-.}"/time-to-8080.csv > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}"-hyperfoil-jbang.log &
+		;;
+	run)
+		kill -s SIGCONT $(pgrep -f HyperfoilWrk)
+		tail --pid=$(pgrep -f HyperfoilWrk) -f /dev/null 
+		;;
+	*)
+		echo "Usage: $0 {prepare|run}"
 		exit 1
-	fi
-fi
-
-# Prepare list of urls to use
-URLS_FIXED_FILE="${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-urls.txt"
-rm -f "$URLS_FIXED_FILE" > /dev/null 2>&1 || true
-URL="http:\/\/localhost:8080"
-sed -e "s/^/$URL/" "$URLS_FILE" > "$URLS_FIXED_FILE"
-
-sleep 1 && pidstat -t -p $(pgrep -f HyperfoilWrk) 1  > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-hyperfoil-pidstat.log" &
-
-echo "${preamble[*]} jbang --java-options=\"-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0\" --java-options=\"-Xmx1G\" --java-options=\"-Xms1G\" --java-options=\"-XX:+UseParallelGC\" --java-options=\"-XX:+AlwaysPreTouch\" src/scripts/drivers/HyperfoilWrk.java -R ${RATE} -d ${DURATION}s -c 50 -o ${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.csv -f ${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" -p "${TEST_OUT_DIR:-.}"/time-to-8080.csv
-"${preamble[@]}" jbang --java-options="-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0" --java-options="-Xmx1G" --java-options="-Xms1G" --java-options="-XX:+UseParallelGC" --java-options="-XX:+AlwaysPreTouch" src/scripts/drivers/HyperfoilWrk.java -R "${RATE}" -d "${DURATION}"s -t 1 -o "${TEST_OUT_DIR:-.}"/"${TEST_TEST_RUNID}".csv -f "${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" -p "${TEST_OUT_DIR:-.}"/time-to-8080.csv > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}"-hyperfoil-jbang.log
+		;;
+esac
