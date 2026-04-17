ba2a2dd91b295d73138926e50c9049a268f3bff5
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-04-17 12:26:29 +0200

    final touches

--- src/scripts/drivers/hyperfoil.sh
+++ src/scripts/drivers/hyperfoil.sh
@@ -46,8 +46,16 @@ case "${CMD}" in
 		URL="http:\/\/localhost:8080"
 		sed -e "s/^/$URL/" "$URLS_FILE" > "$URLS_FIXED_FILE"
 
-		echo "${preamble[*]} jbang --java-options=\"-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0\" --java-options=\"-XX:+DisableExplicitGC\" --java-options=\"-Xmx1G\" --java-options=\"-Xms1G\" --java-options=\"-XX:+UseEpsilonGC\" --java-options=\"-XX:+UnlockExperimentalVMOptions\" --java-options=\"-XX:+AlwaysPreTouch\" src/scripts/drivers/HyperfoilWrk.java -R ${RATE} -d ${DURATION}s -c 50 -o ${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.csv -f ${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" 
-		"${preamble[@]}" jbang --java-options="-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0" --java-options="-XX:+DisableExplicitGC" --java-options="-Xmx1G" --java-options="-Xms1G" --java-options="-XX:+UnlockExperimentalVMOptions" --java-options="-XX:+UseEpsilonGC" --java-options="-XX:+AlwaysPreTouch" src/scripts/drivers/HyperfoilWrk.java -R "${RATE}" -d "${DURATION}"s -t 1 -o "${TEST_OUT_DIR:-.}" -f "${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}"-hyperfoil.log &
+		EXP_OPTS=""
+		#GC="-XX:+UseG1GC"
+		#GC="-XX:+UseSerialGC"
+		#GC="-XX:+UseZGC"
+		#GC="-XX:+UseParallelGC"
+		GC="-XX:+UseEpsilonGC"
+		EXP_OPTS="-XX:+UnlockExperimentalVMOptions"
+
+		echo "${preamble[*]} jbang --java-options="${EXP_OPTS}" --java-options=\"-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0\" --java-options=\"-Dio.hyperfoil.gc.check.enabled=false\" --java-options=\"-XX:+DisableExplicitGC\" --java-options=\"-Xmx1G\" --java-options=\"-Xms1G\" --java-options=\""${GC}"\" --java-options=\"-XX:+AlwaysPreTouch\" src/scripts/drivers/HyperfoilWrk.java -R ${RATE} -d ${DURATION}s -c 50 -o ${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.csv -f ${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" 
+		"${preamble[@]}" jbang --java-options="${EXP_OPTS}" --java-options="-Dio.hyperfoil.cpu.watchdog.idle.threshold=0.0" --java-options="-Dio.hyperfoil.gc.check.enabled=false" --java-options="-XX:+DisableExplicitGC" --java-options="-Xmx1G" --java-options="-Xms1G" --java-options="${GC}" --java-options="-XX:+AlwaysPreTouch" src/scripts/drivers/HyperfoilWrk.java -R "${RATE}" -d "${DURATION}"s -t 1 -o "${TEST_OUT_DIR:-.}" -f "${URLS_FIXED_FILE}" -i "${TEST_TEST_RUNID}" > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}"-hyperfoil.log &
 		pidstat -t -p $(pgrep -f HyperfoilWrk) 1  > "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}-hyperfoil-pidstat.log" &
 		rm -f "${TEST_OUT_DIR:-.}"/hyperfoil.did
 		pgrep -f HyperfoilWrk > "${TEST_OUT_DIR:-.}"/hyperfoil.did
@@ -56,8 +64,7 @@ case "${CMD}" in
 		done
 		;;
 	run)
-		kill -s SIGCONT $(pgrep -f HyperfoilWrk)		
-		wait_for_8080 "${TEST_TEST_RUNID}"
+		kill -s SIGCONT $(pgrep -f HyperfoilWrk)	
 		
 		while [ -f "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.hyperfoil-ready" ]; do
 			sleep 0.5
