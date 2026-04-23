71a4deb2d95b4b5472c0d06c6179240fd632154b
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-04-23 10:20:49 +0200

    Removing useless tests and focus on aot-jar from Quarkus

--- src/scripts/tests/sqpc/quarkus-aot/app.sh
+++ src/scripts/tests/sqpc/quarkus-aot/app.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 source "${TEST_SRC_DIR}"/scripts/appfuncs.sh
 source "${TEST_SUITE_DIR}/shared-vars.sh"
 
-TEST_APP_JAVA=${TEST_APP_JAVA:-21+}
+TEST_APP_JAVA=${TEST_APP_JAVA:-25+}
 
 CMD=$1
-NAME=${2:-sqpc-quarkus-uberjar}
-	
+NAME=${2:-sqpc-quarkus-jar}
+
 case "${CMD}" in
 	start)
-		start_app "${NAME}" "${TEST_BUILDS_DIR}/${REPO_NAME}/quarkus3/quarkus3-uberjar/quarkus3-runner.jar"
+		start_app "${NAME}" "${TEST_BUILDS_DIR}/${REPO_NAME}/quarkus3/quarkus3-jar/quarkus-app/quarkus-run.jar"
 		;;
 	stop)
 		stop_app "${NAME}"
