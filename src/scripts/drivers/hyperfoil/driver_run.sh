13bcc5a155ed843366ef504da882411c24c8fc7e
Tako Schotanus <tako@codejive.org>
2026-04-24 11:42:57 +0200

    fix: if app stops, stop hyperfoil too

--- src/scripts/drivers/hyperfoil/driver_run.sh
+++ src/scripts/drivers/hyperfoil/driver_run.sh
@@ -6,6 +6,19 @@ source "${TEST_SRC_DIR}/scripts/appfuncs.sh"
 
 kill -s SIGCONT $(pgrep -f HyperfoilWrk)	
 
+app_pid=$(get_app_pid "${TEST_TEST_RUNID}")
+
 while [ -f "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.hyperfoil-ready" ]; do
 	sleep 0.5
+	if ! kill -0 "${app_pid}" > /dev/null 2>&1; then
+		echo -e "   - ${BOLD}${RED}✗ Application process has exited unexpectedly${NORMAL}"
+		echo -e "   - ${BOLD}${RED}✗ ${TEST_TEST_RUNID} test application not running${NORMAL}"
+		sleep 2 # give time for output to be flushed
+		echo -e "   - ${RED}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${NORMAL}"
+		outfile="${TEST_OUT_DIR}/${TEST_TEST_RUNID}-app.out"
+		cat "$outfile" 2>/dev/null || true
+		echo -e "   - ${RED}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${NORMAL}"
+		kill -9 "$(pgrep -f HyperfoilWrk)" || true
+		rm -f "${TEST_OUT_DIR:-.}/${TEST_TEST_RUNID}.hyperfoil-ready"  || true
+	fi
 done
