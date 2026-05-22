468fdd523dec0ef69fee87dc7cf1f23f788321f2
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-05-22 11:22:16 +0200

    feat: Four separated tests with no database

--- src/scripts/tests/jpbrw/test.sh
+++ src/scripts/tests/jpbrw/test.sh
@@ -1,14 +1,31 @@
 
-# The test.sh script handles all actions for the test suite.
+# The test.sh script handles all actions for the test.
 # The first argument is the action to perform.
 # Variables defined in shared-vars.sh are available to this script, as well as
-# TEST_SUITE_NAME, TEST_SUITE_DIR.
+# TEST_SUITE_NAME, TEST_SUITE_DIR, TEST_TEST_NAME, TEST_TEST_DIR and TEST_TEST_RUNID.
 
 ACTION=${1:-}
 TESTID=${2:-}
 
+app_jar="${TEST_TEST_CACHE}/wrapper/target/quarkus-app/quarkus-run.jar"
+
 case "${ACTION}" in
+    app_start)
+        start_app "${TESTID}" "${TEST_TEST_CACHE}/wrapper/target/quarkus-app/quarkus-run.jar"
+        ;;
     app_stop)
         stop_app "${TESTID}"
+        ;;
+    setup)
+        REPO_BENCHMARK_URL="https://github.com/ionutbalosin/jvm-performance-benchmarks.git"
+        clone "${REPO_BENCHMARK_URL}" "benchmark"
+        require_java "25"
+        [[ $CLONE_CHANGED -eq 1 || ! -f "${app_jar}" ]] && compile_maven "benchmark"
+
+        REPO_WRAPPER_URL="https://github.com/Delawen/jvm-performance-benchmarks-rest-wrapper.git"
+        clone "${REPO_WRAPPER_URL}" "wrapper"
+        require_java "25+"
+        [[ $CLONE_CHANGED -eq 0 && -f "${app_jar}" ]] && return 0
+        compile_maven "wrapper" "-Dquarkus.package.jar.type=aot-jar"
         ;;
 esac
