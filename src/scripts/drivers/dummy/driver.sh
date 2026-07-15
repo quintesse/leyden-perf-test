8165fae071a8158d51fc74294251d0babc0c558c
Tako Schotanus <tako@codejive.org>
2026-07-15 21:25:30 +0200

    chore: added dummy driver for testing

---
+++ src/scripts/drivers/dummy/driver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+setup() {
+    echo "Dummy driver setup action"
+}
+
+prepare() {
+    echo "Dummy driver prepare action"
+}
+
+run() {
+    echo "Dummy driver run action"
+}
