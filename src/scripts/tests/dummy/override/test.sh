3a2e8aeceb625ebe35f006e867940348a9fd62aa
Tako Schotanus <tako@codejive.org>
2026-07-15 21:18:44 +0200

    chore: added "dummy" test suite for testing

---
+++ src/scripts/tests/dummy/override/test.sh
@@ -0,0 +1,28 @@
+
+
+TESTID=${1:-}
+
+app_setup() {
+    echo "Dummy test app_setup action"
+}
+
+app_start() {
+    echo "Dummy test app_start action"
+}
+
+app_stop() {
+    echo "Dummy test app_stop action"
+}
+
+infra_setup() {
+    echo "Dummy test infra_setup action"
+}
+
+infra_start() {
+    echo "Dummy test infra_start action"
+}
+
+infra_stop() {
+    echo "Dummy test infra_stop action"
+}
+
