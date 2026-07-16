59d09167dd18468c5339d24c20247c33f19eb36f
Tako Schotanus <tako@codejive.org>
2026-07-17 01:11:48 +0200

    refactor: strategies now have much more control

--- src/scripts/strategies/_template/strategy.sh
+++ src/scripts/strategies/_template/strategy.sh
@@ -13,4 +13,28 @@ source "${TEST_SRC_DIR}"/scripts/testfuncs.sh
 # export TEST_STRAT_OPTS=-XX:AOTCache=${TEST_OUT_DIR}/${TEST_TEST_RUNID}-app.aot
 
 # The minimalist implementation (as used by the "normal" strategy) is simply:
-run_all_tests "$1"
+
+# The tests to run is passed as the first parameter
+testpattern=$1
+
+# This is the function that will be run for each test in the suite.
+# It should do all the setup, execution, and teardown necessary for the test.
+function _test() {
+	local result=0
+
+	_run_commands \
+        "infra_setup" \
+        "app_setup" \
+        "driver_setup" \
+        "infra_start/infra_stop" \
+        "driver_prime" \
+        "app_start/app_stop" \
+        "driver_run" -- "${TEST_TEST_RUNID}" || result=$?
+
+	return $result
+}
+
+# This is the toplevel part of the strategy.
+# It runs all tests matching the given pattern using the _test function defined above.
+echo "   - Starting test run..."
+run_for_suite "${testpattern}" "_test"
--- src/scripts/strategies/aot/strategy.sh
+++ src/scripts/strategies/aot/strategy.sh
@@ -9,19 +9,51 @@ if [[ ${TEST_APP_JAVA} -lt 25 ]]; then
 	exit 1
 fi
 
-function prepare_aot_training {
-	echo "   - AOT enabled, starting training run ${TEST_TEST_RUNID}..."
-	export TEST_STRAT_OPTS="-XX:AOTCacheOutput=${TEST_OUT_DIR}/${TEST_SUITE_NAME}-${TEST_TEST_NAME}-app.aot -Xlog:aot+map=trace,aot+map+oops=trace:file=${TEST_OUT_DIR}/${TEST_TEST_RUNID}-aot.map:none:filesize=0 -Xlog:${TEST_LOG_LABEL:-}aot=warning:file=${TEST_OUT_DIR}/${TEST_TEST_RUNID}.log:level,tags"
+testpattern=$1
+
+function _cmds() {
+	_run_commands \
+        "infra_setup" \
+        "app_setup" \
+        "driver_setup" \
+        "infra_start/infra_stop" \
+        "driver_prime" \
+        "app_start/app_stop" \
+        "driver_run" -- "${TEST_TEST_RUNID}"
 }
 
-function prepare_aot_run {
+function _train() {
+	local result=0
+
+	echo "   - AOT enabled, starting training run ${TEST_TEST_RUNID}..."
+	export TEST_STRAT_OPTS="-XX:AOTCacheOutput=${TEST_OUT_DIR}/${TEST_SUITE_NAME}-${TEST_TEST_NAME}-app.aot -Xlog:aot+map=trace,aot+map+oops=trace:file=${TEST_OUT_DIR}/${TEST_TEST_RUNID}-aot.map:none:filesize=0 -Xlog:${TEST_LOG_LABEL:-}aot=warning:file=${TEST_OUT_DIR}/${TEST_TEST_RUNID}.log:level,tags"
+
+	_cmds || result=$?
+
+	return $result
+}
+
+function _test() {
+	local result=0
+
 	echo "   - AOT enabled, starting test run ${TEST_TEST_RUNID}..."
 	export TEST_STRAT_OPTS="-XX:AOTMode=on -XX:AOTCache=${TEST_OUT_DIR}/${TEST_SUITE_NAME}-${TEST_TEST_NAME}-app.aot -Xlog:${TEST_LOG_LABEL:-}aot=warning:file=${TEST_OUT_DIR}/${TEST_TEST_RUNID}.log:level,tags"
+
+	_cmds || result=$?
+
+	if [[ -f "${TEST_OUT_DIR}/${TEST_TEST_RUNID}-profile.jfr" ]]; then
+		# If we have JFR files, process them
+		generate_profiling_results "${TEST_TEST_RUNID}"
+	fi
+
+	return $result
 }
 
 # First we do a training run to create an AOT cache
 # We mark all output files with a "training" tag to differentiate them from the real run
-run_all_tests "$1" "training" "prepare_aot_training"
+echo "   - Starting training..."
+run_for_suite "${testpattern}" "_train" "training"
 
 # Now run again using the AOT cache
-run_all_tests "$1" "aot" "prepare_aot_run"
+echo "   - Starting test run..."
+run_for_suite "${testpattern}" "_test" "aot"
