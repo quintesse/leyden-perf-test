87987c2a6fa2a19ca99f532961584c6491f49e61
Tako Schotanus <tako@codejive.org>
2026-07-17 01:11:48 +0200

    refactor: strategies now have much more control

--- src/scripts/strategies/normal/strategy.sh
+++ src/scripts/strategies/normal/strategy.sh
@@ -4,5 +4,26 @@ set -euo pipefail
 
 source "${TEST_SRC_DIR}"/scripts/testfuncs.sh
 
+function _test() {
+	local result=0
+
+	_run_commands \
+        "infra_setup" \
+        "app_setup" \
+        "driver_setup" \
+        "infra_start/infra_stop" \
+        "driver_prime" \
+        "app_start/app_stop" \
+        "driver_run" -- "${TEST_TEST_RUNID}" || result=$?
+
+	if [[ -f "${TEST_OUT_DIR}/${TEST_TEST_RUNID}-profile.jfr" ]]; then
+		# If we have JFR files, process them
+		generate_profiling_results "${TEST_TEST_RUNID}"
+	fi
+
+	return $result
+}
+
 echo "   - Starting test run..."
-run_all_tests "$1"
+testpattern=$1
+run_for_suite "${testpattern}" "_test"
